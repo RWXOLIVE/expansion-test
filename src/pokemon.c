fb6b04abd0e6d533ac5a396ff79fe7e9bf310141
Ruben <rubenwhite3005@gmail.com>
2025-02-26 17:40:53 +0000

    rare candy goes over lvl cap

--- src/pokemon.c
+++ src/pokemon.c
@@ -3746,6 +3746,7 @@ const u32 sExpCandyExperienceTable[] = {
 };
 
 // Returns TRUE if the item has no effect on the Pokémon, FALSE otherwise
+// Returns TRUE if the item has no effect on the Pokémon, FALSE otherwise
 bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 moveIndex, bool8 usedByAI)
 {
     u32 dataUnsigned;
@@ -3817,6 +3818,30 @@ bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mov
                 if (param == 0) // Rare Candy
                 {
                     dataUnsigned = gExperienceTables[gSpeciesInfo[GetMonData(mon, MON_DATA_SPECIES, NULL)].growthRate][GetMonData(mon, MON_DATA_LEVEL, NULL) + 1];
+                }
+                else if (param == LEVEL_CAP)
+                {
+                    u32 levelUpThreshold = GetCurrentLevelCap();
+                    u32 blockLevelUp = FALSE;
+                    u32 monLevel = GetMonData(mon, MON_DATA_LEVEL, NULL);
+                    const struct Evolution *evolutions = GetSpeciesEvolutions(GetMonData(mon, MON_DATA_SPECIES, NULL));
+                    for (u32 evo = 0; evolutions[evo].method != EVOLUTIONS_END; evo++)
+                    {
+                        u32 evoLevelUp = evolutions[evo].param;
+                        if (evoLevelUp <= monLevel && evoLevelUp != 0)
+                        {
+                            dataUnsigned = gExperienceTables[gSpeciesInfo[GetMonData(mon, MON_DATA_SPECIES, NULL)].growthRate][GetMonData(mon, MON_DATA_LEVEL, NULL) + 1];
+                            blockLevelUp = TRUE;
+                            break;
+                        }
+                        // Prevents Looping if no evo methos was found
+                        if (SanitizeSpeciesId(evolutions[evo].targetSpecies) == SPECIES_NONE)
+                            break;
+                        if (evoLevelUp < levelUpThreshold && monLevel < levelUpThreshold)
+                            levelUpThreshold = evoLevelUp;
+                    }
+                    if (!blockLevelUp)
+                        dataUnsigned = gExperienceTables[gSpeciesInfo[GetMonData(mon, MON_DATA_SPECIES, NULL)].growthRate][levelUpThreshold];
                 }
                 else if (param - 1 < ARRAY_COUNT(sExpCandyExperienceTable)) // EXP Candies
                 {
